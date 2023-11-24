1d86ac239d11c5034bd5cba5b07e1864a905f3b0
Raivo-Salaginovs <raivomadona@inbox.lv>
2023-11-24 14:31:46 +0200

    2.uzd izdarīts

--- 2.uzdevums.py
+++ 2.uzdevums.py
@@ -1,3 +1,6 @@
 #Izveidojiet Python programmu, kas izmanto while ciklu, lai atrastu pirmo skaitli, kura kvadrāts ir lielāks par 1000.
 #Izmantojiet GIT, lai izsekotu kodu un veidotu komitus.
 
+skaitlis=[1,2,3,4,5,6,7,8,9,10]
+kvadrati=list(filter(lambda x:x%2==0 or x%3==0, skaitlis))
+print(kvadrati)
